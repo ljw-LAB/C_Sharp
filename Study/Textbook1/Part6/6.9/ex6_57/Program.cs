9bb0a908dae6dbe5de589ca3885546fa009b5436
ljw-LAB <su5099@naver.com>
2021-06-04 09:58:32 +0900

    Part6 Commit  - Part6.9 ~ 6.10 ex6_57 Commit

---
+++ Study/Textbook1/Part6/6.9/ex6_57/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ex6_57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            Console.WriteLine("Current Domain Name: " + currentDomain.FriendlyName);
+            foreach(Assembly asm in currentDomain.GetAssemblies())
+            {
+                Console.WriteLine(" " + asm.FullName);
+                // foreach(Module module in asm.GetModules())
+                // {
+                //     Console.WriteLine(" " + module.FullyQualifiedName);
+                //     foreach(Type type in module.GetTypes())
+                //     {
+                //         Console.WriteLine(" " + type.FullName);
+                //     }
+                // } 
+                foreach(Type type in asm.GetTypes())
+                {
+                    Console.WriteLine("   " + type.FullName);
+                    foreach(MemberInfo memberInfo in type.GetMembers())
+                    {
+                        Console.WriteLine("   " + memberInfo.Name);
+                    }
+                }
+            } 
+        }
+    }
+}
