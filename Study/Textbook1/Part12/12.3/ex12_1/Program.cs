6c077db03143004f674e3a214e5ec2964a5f9aca
ljw-LAB <su5099@naver.com>
2021-06-15 08:46:41 +0900

    Part12 Commit  - Part12.1 ~ 12.10 ex12_1 ~ ex12_2 Commit

---
+++ Study/Textbook1/Part12/12.3/ex12_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex12_1
+{
+    public class IntResult
+    {
+        public bool Parsed {get; set;}
+        public int Number {get; set;}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            IntResult result = pg.ParseInteger("15");
+
+            Console.WriteLine(result.Parsed);
+            Console.WriteLine(result.Number);
+        }
+
+        IntResult ParseInteger(string text)
+        {
+            IntResult result = new IntResult();
+            try
+            {
+                result.Number = Int32.Parse(text);
+                result.Parsed = true;
+            }
+            catch
+            {
+                result.Parsed = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Study/Textbook1/Part12/12.7/ex12_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+// NuGet 패키지에서 ValueTuple을 설치
+// NuGet 패키지에서 Threading.Tasks.Extensions
+
+namespace ex12_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task<(string, int tid)> result = FileReadAsync(@"C:\windows\system32\drivers\etc\HOSTS");
+            //int tid = Thread.CurrentThread.ManagedThreadId;
+            //Console.WriteLine($"MainThreadID: {tid}, AsytThreadID: {result.Result.tid}");
+
+            //Task<(string, int tid)> result =
+            //    FileReadAsync(@"C:\windows\system32\drivers\etc\HOSTS");
+
+            //int tid = Thread.CurrentThread.ManagedThreadId;
+            //Console.WriteLine($"MainThreadID: {tid}, AsyncThreadID: {result.Result.tid}");
+
+            {
+                ValueTask<(string, int tid)> result = FileReadAsync2(@"C:\windows\system32\drivers\etc\HOSTS");
+
+                int tid = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"MainThreadID: {tid}, AsyncThreadID: {result.Result.tid}");
+            }
+
+            {
+                ValueTask<(string, int tid)> result = FileReadAsync2(@"C:\windows\system32\drivers\etc\HOSTS");
+
+                int tid = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"MainThreadID: {tid}, AsyncThreadID: {result.Result.tid}");
+            }
+
+        }
+
+        //private static async Task<(string, int)> FileReadAsync(string filePath)
+        //{
+        //    string fileText = await ReadAllTextAsync(filePath);
+        //    return (fileText, Thread.CurrentThread.ManagedThreadId);
+        //}
+
+        static string _fileContents = string.Empty;
+        private static async Task<(string, int)> FileReadAsync(string filePath)
+        {
+            if(string.IsNullOrEmpty(_fileContents) == false)
+            {
+                return (_fileContents, Thread.CurrentThread.ManagedThreadId);
+            }
+
+            _fileContents = await ReadAllTextAsync(filePath);
+            return (_fileContents, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static async ValueTask<(string, int)> FileReadAsync2(string filePath)
+        {
+            if (string.IsNullOrEmpty(_fileContents) == false)
+            {
+                return (_fileContents, Thread.CurrentThread.ManagedThreadId);
+            }
+
+            _fileContents = await ReadAllTextAsync(filePath);
+            return (_fileContents, Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static Task<string> ReadAllTextAsync(string filePath)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                return File.ReadAllText(filePath);
+            });
+        }
+    }
+}
