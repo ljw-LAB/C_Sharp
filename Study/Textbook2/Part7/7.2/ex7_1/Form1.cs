6ed44667735e2b99e704af38f17dcacba77a9082
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-07-21 22:13:36 +0900

    Textbook2 Part7 Commit  - Part7 7.2  - ex7_1 ~  ex7_3 Commit

---
+++ Study/Textbook2/Part7/7.2/ex7_1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex7_1
+{
+    public partial class Form1 : Form
+    {
+  
+        public Form1()
+        {
+            InitializeComponent();
+            startDateTime = DateTime.Now;
+        }
+
+        DateTime startDateTime;
+        public DateTime GetStartDateTime()
+        {
+            return startDateTime;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetStartDateTime().ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Study/Textbook2/Part7/7.2/ex7_2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex7_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("This form is loaded");
+        }
+
+        private void Form1_Closed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("This form is closed");
+            // Closed 없음, FormClosed로 찾아야함, Closing도 동일
+        }
+    }
+}
