7f1950243396c72c61d3743ff8595a5cf51c506d
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-07-22 13:19:06 +0900

    Textbook2 Part7 Commit  - Part7 7.3  - ex7_4 ~  ex7_6 Commit

---
+++ Study/Textbook2/Part7/7.3/ex7_5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex7_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(0, 255), r.Next(0,255), r.Next(0,255));
+            g.FillRectangle(new SolidBrush(c), e.ClipRectangle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+    }
+}
