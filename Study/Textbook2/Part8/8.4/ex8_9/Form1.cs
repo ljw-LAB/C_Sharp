863dcbba2678f2303103b8203c5e141c000568e7
ljw-LAB <su5099@naver.com>
2021-07-26 17:38:15 +0900

    Textbook2 Part8 Commit  - Part8 8.4  - ex8_7 ~  ex8_9 Commit

---
+++ Study/Textbook2/Part8/8.4/ex8_9/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex8_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strTemp = " ";
+            foreach(object obj in checkedListBox1.CheckedItems)
+            {
+                strTemp += obj.ToString();
+                strTemp += " ";
+            }
+            MessageBox.Show("당신의 취미는" + strTemp + "입니다");
+        }
+    }
+}
