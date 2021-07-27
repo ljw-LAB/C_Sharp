a0c86e9fdb4f0805bbc8f506744d73c936a8e299
ljw-LAB <su5099@naver.com>
2021-07-27 10:13:56 +0900

    Textbook2 Part9 Commit  - Part9 9.2 ~ 9.3  - ex9_3 ~  ex9_11 Commit

---
+++ Study/Textbook2/Part9/9.2/ex9_5/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex9_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int i;
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            i = i < 64 ? i += 16 : 0;
+            this.Text = ((MessageBoxIcon)i).ToString();
+            MessageBox.Show("MessageBoxIcon", "TitleBar", MessageBoxButtons.OKCancel, (MessageBoxIcon)i);
+        }
+    }
+}
