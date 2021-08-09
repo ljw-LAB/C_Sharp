bdac059e75d1f6cac358fb03eb98d05e78010ad6
ljw-LAB <su5099@naver.com>
2021-08-09 14:08:45 +0900

    Textbook3 Part1 Commit  - Part1  - ex_001 ~ ex_010 Commit

---
+++ Study/Textbook3/Part1/ex_008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 이하의 소수 : {0}, {1}, {2}, {3}", 2 ,3, 5, 7, 9);
+
+            string primes;
+
+            primes = String.Format("10 이하의 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+            Console.WriteLine(primes);
+        }
+    }
+}
---
+++ Study/Textbook3/Part1/ex_010/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Standard Numeric Format Specifiers");
+            Console.WriteLine(
+                "(C) Currency:........{0:C}\n" +
+                "(D) Currency:........{0:D}\n" +
+                "(E) Currency:........{1:E}\n" +
+                "(F) Currency:........{1:F}\n" +
+                "(G) Currency:........{0:G}\n" +
+                "(N) Currency:........{0:N}\n" +
+                "(P) Currency:........{1:P}\n" +
+                "(R) Currency:........{1:R}\n" +
+                "(X) Currency:........{0:X}\n", -12345678, -1234.5678f
+
+            );
+        }
+    }
+}
