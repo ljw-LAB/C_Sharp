b66bbbff78899e27a141d885955fa05e4bc60f56
ljw-LAB <su5099@naver.com>
2021-08-11 16:02:54 +0900

    Textbook3 Part1 Commit  - Part1  - ex_021 ~ ex_030 Commit

---
+++ Study/Textbook3/Part1/ex_027/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "bikang";
+            string date = DateTime.Today.ToShortDateString();
+
+            string strPlus = "Hello " + username + ". Today is " + date + ".";
+            Console.WriteLine(strPlus);
+
+            string strFomat = String.Format("Hello {0}. Today is {1}.", username, date);
+            Console.WriteLine(strFomat);
+
+            string strInterpolation = $"Hello {username}. Today is {date}";
+            Console.WriteLine(strInterpolation);
+
+            string strConcat = String.Concat("Hello ", username, ". Today is ", date, ".");
+            Console.WriteLine(strConcat);
+
+            string[] animals = {"mouse", "cow", "tiger", "rabbit", "dragon"};
+            string s = String.Concat(animals);
+            Console.WriteLine(s);
+            s = String.Join(", ", animals);
+            Console.WriteLine(s);
+        }
+    }
+}
