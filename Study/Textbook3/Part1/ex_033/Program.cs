4180f1bbf951f127f3b3ec10db255a78b04fa002
ljw-LAB <su5099@naver.com>
2021-08-12 13:33:22 +0900

    Textbook3 Part1 Commit  - Part1  - ex_031 ~ ex_040 Commit

---
+++ Study/Textbook3/Part1/ex_033/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_033
+{
+    class ConstEx
+    {
+        public const int number = 3;
+    }
+
+    class ReadonlyEx
+    {
+        public readonly int number = 10;
+        public ReadonlyEx()
+        {
+            number = 20;
+        }
+
+        public ReadonlyEx(int n)
+        {
+            number =  n;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConstEx.number);
+
+            ReadonlyEx inst1 = new ReadonlyEx();
+            Console.WriteLine(inst1.number);
+
+            ReadonlyEx inst2 = new ReadonlyEx(100);
+            Console.WriteLine(inst2.number);
+            
+        }
+    }
+}
