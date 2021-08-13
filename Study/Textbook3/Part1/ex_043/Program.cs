b5fcd35cbc66727c5dba45d7265b99b8f93c37b5
ljw-LAB <su5099@naver.com>
2021-08-13 13:41:37 +0900

    Textbook3 Part1 Commit  - Part1  - ex_041 ~ ex_050 Commit

---
+++ Study/Textbook3/Part1/ex_043/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            for(int i = 1; i<= 100; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("1부터 100까지 숫자의 합은 {0}", sum);
+
+            int sum2 = 0;
+            for(int x =1; x<= 100; x++)
+            {
+                if(x % 2 == 1)
+                    sum += x;
+            }
+            Console.WriteLine("1부터 100까지 홀수의 합은 {0}", sum2);
+
+            double sum3 =0;
+            for(int x = 1; x<= 100; x++)
+            {
+                sum3 += 1.0/x;
+            }
+            Console.WriteLine("1부터 100까지 역수의 합은 {0}", sum3);
+
+        }
+    }
+}
