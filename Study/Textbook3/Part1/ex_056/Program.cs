d5856f946bd332550bf7d549daa45152c9f31900
ljw-LAB <su5099@naver.com>
2021-08-18 16:03:18 +0900

    Textbook3 Part1, 2 Commit  - Part1, 2  - ex_051 ~ ex_060 Commit

---
+++ Study/Textbook3/Part1/ex_056/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_056
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= 2*i-1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for(int i = 5; i >= 1; i--)
+            {
+                for(int j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= 5-i; j++)
+                    Console.Write(" ");
+                for(int j =1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for(int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= 5-i; j++)
+                    Console.Write(" ");
+                for(int j = 1; j <= 2*i-1;j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for(int i = 5; i >= 1; i--)
+            {
+                for(int j = 1; j <= 5-i; j++)
+                    Console.Write(" ");
+                for(int j = 1; j <= 2*i-1;j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }            
+        }
+    }
+}
