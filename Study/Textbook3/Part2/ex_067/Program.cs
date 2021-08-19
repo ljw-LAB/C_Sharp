0024bd099179a6c54638613350487a614a39b451
ljw-LAB <su5099@naver.com>
2021-08-19 16:22:56 +0900

    Textbook3 Part2 Commit  - Part2  - ex_061 ~ ex_070 Commit

---
+++ Study/Textbook3/Part2/ex_067/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_067
+{
+    class Date
+    {
+        public int year, month, day;
+
+        public static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 &&(year % 100 != 0 || year % 400 == 0);
+        }
+
+        static int[] days = {0, 31, 69 , 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+        public int DayofYear()
+        {
+            return days[month - 1] + day + (month > 2 && IsLeapYear(year) ? 1 : 0);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date xmas = new Date();
+
+            xmas.year = 2018;
+            xmas.month = 12;
+            xmas.day = 25;
+
+            Console.WriteLine("xmas : {0}/{1}/{2}는 {3}일째 되는 날이다", xmas.year, xmas.month, xmas.day, xmas.DayofYear());
+
+            if(Date.IsLeapYear(2018) == true)
+            {
+                Console.WriteLine("2018년 윤년입니다.");
+            }
+            else
+            {
+                Console.WriteLine("2018년 평년입니다.");
+            }
+        }
+    }
+}
---
+++ Study/Textbook3/Part2/ex_070/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 30, c = 20;
+            Program x = new Program();
+            //Console.WriteLine("가장 큰 수는 {0}", x.Larger(x.Larger(a, b), c));
+            Console.WriteLine("가장 큰 수는 {0}", Larger(Larger(a, b), c));
+
+        }
+
+        // private int Larger(int a, int b)
+        // {
+        //     return (a >= b) ? a : b;
+        // }
+        private static int Larger(int a, int b)
+        {
+            return (a >= b) ? a : b;
+        }
+    }
+}
