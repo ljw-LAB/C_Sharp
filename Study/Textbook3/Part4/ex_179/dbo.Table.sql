86e0d80120ce8f420a3c956f05be1e5f56cc12ab
ljw-LAB <su5099@naver.com>
2021-09-06 15:12:24 +0900

    Textbook3 Part4 Commit  - Part4  - ex_171 ~ ex_181 Commit

---
+++ Study/Textbook3/Part4/ex_179/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LoginTable]
+(
+	[Id]	   INT			IDENTITY(1,1) NOT NULL, 
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+
+);
